50a4dce03991fa5db60b33bb788c7a0be71ceae8
vidalmamanijordan <vidal_mamani@upeu.edu.pe>
2022-03-11 16:24:54 -0500

    Componentes blade

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,4 @@
+<div {{ $attributes->merge(['class' => "bg-$color-100 border-l-4 border-$color-500 text-$color-700 p-4" ]) }} role="alert">
+    <p class="font-bold">{{ $title }}</p>
+    <p>{{ $slot }}</p>
+</div>
